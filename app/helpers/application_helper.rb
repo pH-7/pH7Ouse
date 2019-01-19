ffecfb40e4615766476f72ce30edda7a09bec110
Pierre-Henry Soria <hi@ph7.me>
2019-01-20 01:34:37 +1300

    Add gravatar user image to navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    gravatar_size = 150;
+    "https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=wavatar&s=#{gravatar_size}&r=g"
+  end
 end
